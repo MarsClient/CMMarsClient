2dfb3e37c90eb9e9c245773e4c1abfff1e9f9f0d
MachoChen <406028609@qq.com>
2014-06-21 22:44:25 +0800

    Add

--- MarsClient/Assets/Scripts/Net/Main.cs
+++ MarsClient/Assets/Scripts/Net/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent (typeof (FpsCounter))]
 public class Main : MonoBehaviour 
 {
 	public static Main Instance;
---
+++ MarsClient/Assets/Scripts/UI/Chat/ChatClose.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatClose : MonoBehaviour {
+
+	void OnClick ()
+	{
+
+	}
+}
