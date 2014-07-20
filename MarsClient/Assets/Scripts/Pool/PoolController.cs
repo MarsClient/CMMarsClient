c2f3a8e44054d9270730c90c3d1d9447892b53d7
MachoChen <406028609@qq.com>
2014-07-20 20:07:38 +0800

    add pool

---
+++ MarsClient/Assets/Scripts/Pool/PoolController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoolController : MonoBehaviour {
+
+	private string mPath;
+	private GameObject m_go;
+	public void Init (string path)
+	{
+		this.mPath = path;
+		this.m_go = gameObject;
+	}
+
+	public void Release ()
+	{
+		PoolManager.Instance.Release (mPath, m_go);
+	}
+
+}
---
+++ MarsClient/Assets/Scripts/Pool/PoolManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Pool manager.
+/// this class is One only in fight scene
+/// when load new scene,this class will release.
+/// </summary>
+
+public class PoolManager : MonoBehaviour {
+
+	public static PoolManager Instance;
+	void Awake () { Instance = this; }
+	void OnDestroy() { Instance = null; }
+
+	private Dictionary<string, LinkedList<GameObject>> pools = new Dictionary<string, LinkedList<GameObject>>();
+
+	public GameObject LoadGameObject (string path, Transform target)
+	{
+		GameObject m_go = null;
+		LinkedList<GameObject> goList = null;
+
+		if (pools.TryGetValue (path, out goList) == false)
+		{
+			goList = new LinkedList<GameObject> ();
+			pools.Add (path, goList);
+		}
+
+		if (goList.Count > 0)
+		{
+			m_go = goList.First.Value;
+			goList.RemoveFirst ();
+			pools[path] = goList;
+		}
+		else
+		{
+			m_go = GameObject.Instantiate(Resources.Load (path) as GameObject) as GameObject;
+			PoolController pc = m_go.AddComponent<PoolController> ();
+			pc.Init (path);
+		}
+		if (target != null)
+		{
+			m_go.transform.position = target.position;
+			m_go.transform.rotation = target.rotation;
+		}
+		m_go.SetActive (true);
+		return m_go;
+	}
+
+	public void Release (string path, GameObject go)
+	{
+		LinkedList<GameObject> goList = null;
+		
+		if (pools.TryGetValue (path, out goList) == false)
+		{
+			goList = new LinkedList<GameObject>();
+		}
+		goList.AddLast (go);
+		go.SetActive (false);
+		pools[path] = goList;
+	}
+}
