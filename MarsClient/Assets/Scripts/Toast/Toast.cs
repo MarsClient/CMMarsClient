b9f0d3542d0c3fe04a791face2e5a36ae8ed7960
MachoChen <406028609@qq.com>
2014-07-26 01:43:28 +0800

    Fixed tweenDuration

--- MarsClient/Assets/Scripts/Toast/Toast.cs
+++ MarsClient/Assets/Scripts/Toast/Toast.cs
@@ -7,7 +7,7 @@ public class Toast : MonoBehaviour {
 	private static Toast instance;
 	void Awake () {if (instance == null) { instance = this; DontDestroyOnLoad (gameObject); } else if (instance != this) Destroy (gameObject); }
 
-	private GameObject[] allItems = new GameObject[5];// = new List<GameObject> (5);
+	private GameObject[] allItems = new GameObject[5];
 	private Queue<string> queue = new Queue<string>();
 
 	public GameObject parent;
@@ -45,12 +45,10 @@ public class Toast : MonoBehaviour {
 	private void ShowText (string text, bool isQueue)
 	{
 		if (isQueue) { queue.Enqueue (text); }
-		int count = disableNum;//GetComponentsInChildren <UILabel>().Length;//allItems.Count;
-//		Debug.Log (count);
+		int count = disableNum;
 		if (count < maxCount)
 		{
-			//Debug.Log (text);
-			if (isQueue) { string message = queue.Dequeue (); /*Debug.LogError (message)*/; }
+			if (isQueue) { string message = queue.Dequeue (); }
 			StartCoroutine (delayTween (AddChild (text)));
 		}
 	}
@@ -59,25 +57,32 @@ public class Toast : MonoBehaviour {
 	{
 		yield return new WaitForSeconds (lastTime);
 
-		TweenAlpha.Begin (go, tweenDuration, 0);//.ignoreTimeScale = false;
+		TweenAlpha.Begin (go, tweenDuration, 0);
 		yield return new WaitForSeconds (tweenDuration);
 		go.SetActive (false);
-		//DestroyImmediate (go);
 		if (queue.Count > 0)
 		{
 			string message = queue.Dequeue (); 
-			//Debug.LogError (message);
+
 			ShowText (message, false);
 		}
-		//StartCoroutine (delayDestory (go));
 	}
 
 	GameObject AddChild (string text)
 	{
+		for (int j = 0; j < maxCount; j++)
+		{
+			if (allItems[j] != null && allItems[j].activeSelf == true)
+			{
+				Vector3 pos = allItems[j].transform.localPosition;
+				allItems[j].transform.localPosition = new Vector3 (pos.x, Mathf.Min (pos.y + distance, distance * (maxCount - 1)));
+			}
+		}
+
+
 		GameObject go = null;
 		for (int i = 0; i < maxCount; i++)
 		{
-			//Debug.Log ("i = " + i);
 			bool isNeedSet = (allItems[i] == null || allItems[i].activeSelf == false);
 			if (allItems[i] == null)
 			{
@@ -93,8 +98,8 @@ public class Toast : MonoBehaviour {
 			if (isNeedSet)
 			{
 				UILabel label = allItems[i].GetComponentInChildren<UILabel> ();
-				allItems[i].transform.localPosition = new Vector3 (0, distance * i, 0);
-//				Debug.LogError (allItems[i] + "___" + allItems[i].activeSelf);
+				//allItems[i].transform.localPosition = new Vector3 (0, distance * i, 0);
+				allItems[i].transform.localPosition = Vector3.zero;
 				label.alpha = 0;
 				TweenAlpha.Begin (allItems[i], tweenDuration / 2, 1.0f);
 				label.text = text;
@@ -104,20 +109,19 @@ public class Toast : MonoBehaviour {
 		return go;
 	}
 
-//	int i = 0;
-//	void OnGUI ()
-//	{
-//		//Debug.LogError (parent.transform.childCount);
-//		if (GUILayout.Button ("hahahahhahahah"))
-//		{
-//			string s = "test" + i++.ToString ();
-//			ShowText (s);
-//			//Debug.Log (s);
-//		}
-//		if (GUILayout.Button ("reset"))
-//		{
-//			i = 0;
-//		}
+	int i = 0;
+	void OnGUI ()
+	{
+		if (GUILayout.Button ("hahahahhahahah"))
+		{
+			string s = "hahahahhahahahhahahahhahahahhahahahhahahah" + i++.ToString ();
+			ShowText (s);
+			//Debug.Log (s);
+		}
+		if (GUILayout.Button ("reset"))
+		{
+			i = 0;
+		}//
 //		if (GUILayout.Button ("hahahahhahahah"))
 //		{
 //			new DialogContent ()
@@ -136,7 +140,7 @@ public class Toast : MonoBehaviour {
 //					.SetDelegateBtn (DialogBtnEvent)
 //					.ShowWaiting ();
 //		}
-//	}
+	}
 //
 //	void DialogBtnEvent (bool yes)
 //	{
