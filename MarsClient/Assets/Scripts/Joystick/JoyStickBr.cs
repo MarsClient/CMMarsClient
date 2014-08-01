5bc0145383f3b1785ae8948a8d316102a229c8cb
MachoChen <406028609@qq.com>
2014-08-02 02:47:26 +0800

    Fixed JoyStickBr

---
+++ MarsClient/Assets/Scripts/Joystick/JoyStickBr.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoyStickBr : MonoBehaviour {
+
+	public float duration = 0.25f;
+	public float startScale = 1.5f;
+	public float startAlpha = 1.0f;
+
+	public UISprite foreground;
+	public UISprite background;
+
+	void OnClick ()
+	{
+
+	}
+
+	public void SetIcon (string spName)
+	{
+		background.spriteName = spName;
+		foreground.spriteName = spName;
+		StartTween ();
+	}
+
+	private void StartTween ()
+	{
+		background.alpha = startAlpha;
+		background.transform.localScale = Vector3.one;
+
+		TweenAlpha.Begin (background.gameObject, duration, 0);
+		TweenScale.Begin (background.gameObject, duration, Vector3.one * startScale );
+
+	}
+
+	void OnGUI ()
+	{
+		if (GUILayout.Button ("xxxxxxxxxxxxxxxxxxxxxxx"))
+		{
+			StartTween ();
+		}
+	}
+}
