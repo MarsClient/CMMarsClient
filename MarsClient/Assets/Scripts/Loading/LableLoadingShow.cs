277b932420ec591c484bda47f1f3d2da06a61167
MachoChen <406028609@qq.com>
2014-08-14 22:15:00 +0800

    Add loadingShow label

--- MarsClient/Assets/Scripts/Loading/LableLoadingShow.cs
+++ MarsClient/Assets/Scripts/Loading/LableLoadingShow.cs
@@ -5,33 +5,33 @@ public class LableLoadingShow : MonoBehaviour {
 
 	UILabel m_Label;
 //
-//	void Start ()
-//	{
-//		m_Label = GetComponent<UILabel> ();
-//		InvokeRepeating ("UpdateText", 0, 0.25f);
-//	}
-//
-//	int i = 0;
-//	void UpdateText ()
-//	{
-//
-//		if (i == 0)
-//		{
-//			m_Label.text = "Loading";
-//		}
-//		if (i == 1)
-//		{
-//			m_Label.text = "Loading.";
-//		}
-//		if (i == 2)
-//		{
-//			m_Label.text = "Loading..";
-//		}
-//		if (i == 3)
-//		{
-//			m_Label.text = "Loading...";
-//			i = 0;
-//		}
-//		++i;
-//	}
+	void Start ()
+	{
+		m_Label = GetComponent<UILabel> ();
+		InvokeRepeating ("UpdateText", 0, 0.25f);
+	}
+
+	int i = 0;
+	void UpdateText ()
+	{
+
+		if (i == 0)
+		{
+			m_Label.text = "Loading";
+		}
+		if (i == 1)
+		{
+			m_Label.text = "Loading.";
+		}
+		if (i == 2)
+		{
+			m_Label.text = "Loading..";
+		}
+		if (i == 3)
+		{
+			m_Label.text = "Loading...";
+			i = 0;
+		}
+		++i;
+	}
 }
