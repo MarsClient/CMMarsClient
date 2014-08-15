81bb290f0528c85d35f26c0afc3c0303fa5b63e7
MachoChen <406028609@qq.com>
2014-08-15 22:39:30 +0800

    Fixed CreatMonster

---
+++ MarsClient/Assets/Editor/CreatMonsterEdit.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor (typeof (CreatMonster))]
+public class CreatMonsterEdit : Editor {
+
+	/*
+	 *@see script.id is tranform's name
+	 *@see script.x is tranform.pos.x
+	 *@see script.z is tranform.pos.z
+	*/
+	public override void OnInspectorGUI () 
+	{
+		CreatMonster script = target as CreatMonster;
+
+		DrawDefaultInspector ();
+		EditorGUI.indentLevel = 1;
+
+		script.id = script.transform.name;//EditorGUILayout.LabelField ("id", script.transform.name);
+		script.x = script.transform.position.x;
+		script.z = script.transform.position.z;
+
+		if (GUILayout.Button ("Modified spells is invalid, when isBoss is False!!!","HelpBox"))
+		{}
+
+		if (GUI.changed) EditorUtility.SetDirty (target);
+	}
+
+
+
+	/*
+	 *@Creat operator
+	 */
+	[MenuItem("Game Editor/Create MonsterPos", false, 9)]
+	static void CreatMonsterPosJson ()
+	{
+		Object[] SelectedAsset = Selection.GetFiltered (typeof(Object), SelectionMode.DeepAssets);
+		try
+		{
+			string sourcePath = AssetDatabase.GetAssetPath (SelectedAsset[0]);
+			if (sourcePath.Contains (".unity"))
+			{
+				string targetPath = sourcePath.Replace (".unity", ".txt");
+				Fight fight = new Fight ();
+				fight.gameMonsters = new Dictionary<string, GameMonster[]> ();
+				for (int i = 100000;;i++)
+				{
+					string tra_Name = i.ToString ();
+					GameObject go = GameObject.Find (tra_Name);
+					if (go != null)
+					{
+						
+						Transform json_Go = go.transform.FindChild ("MonsterPos");
+						fight.gameMonsters.Add (tra_Name, new GameMonster[json_Go.childCount]);
+						int idx = 0;
+						foreach (CreatMonster creatMonster in json_Go.GetComponentsInChildren<CreatMonster>())
+						{
+
+							GameMonster gameMonster = new GameMonster ();
+							gameMonster.id = long.Parse (creatMonster.id);
+							gameMonster.type = creatMonster.type;
+							gameMonster.level = creatMonster.level;
+							gameMonster.x = creatMonster.x;
+							gameMonster.z = creatMonster.z;
+							gameMonster.hp = creatMonster.hpMax;
+							gameMonster.hpMax = creatMonster.hpMax;
+							fight.gameMonsters[tra_Name][idx] = gameMonster;
+//							string[] arr = tra.name.Split ('_');
+//							GameMonster gameMonster = new GameMonster ();
+//							gameMonster.id = int.Parse(arr[0]);
+//							gameMonster.type = arr[1];
+//							gameMonster.level = int.Parse (arr[2]);
+//							gameMonster.x = tra.position.x;
+//							gameMonster.z = tra.position.z;
+//							gameMonster.hp = 1000;
+//							gameMonster.hpMax = 1000;
+//							fight.gameMonsters[tra_Name][idx] = gameMonster;
+							idx++;
+							
+						}
+					}
+					else break;
+				}
+				
+				
+				FileStream fs = new FileStream (targetPath, FileMode.Create, FileAccess.Write);
+				StreamWriter sw = new StreamWriter(fs);
+				sw.WriteLine (JsonConvert.SerializeObject (fight));
+				sw.Close ();
+				fs.Close ();
+				Debug.Log ("CREAT " + targetPath + " SUCCESSFUL");
+				AssetDatabase.Refresh ();
+			}
+		}
+		catch (System.Exception e)
+		{
+			Debug.LogError (e);
+		}
+		
+	}
+}
---
+++ MarsClient/Assets/Scripts/CreatMonster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatMonster : MonoBehaviour {
+
+	public string id;
+	public string type;
+//	public string name;
+//	public string desc;
+//	public string icon;
+//	public string model;
+//	public int value;
+//	public int gold;
+//	public int gem;
+	public int level;
+//	public float hp;
+	public float hpMax;
+	
+	public float x;
+	public float z;
+//	public float xRo;
+//	public float zRo;
+//	public float action;
+	public bool isBoss;
+
+	/// <summary>
+	/// The spells are boss's.
+	/// e.g spell1  spell2...
+	/// </summary>
+	public string[] spells;
+	//public string[] spells;
+}
